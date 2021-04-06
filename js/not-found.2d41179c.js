0724700d74fdd59ac4578c9efb1611d3b59c8168
Istomina Asya <istomina.asia@yandex.ru>
2021-04-06 15:22:16 +0300

    Updates

---
+++ js/not-found.2d41179c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["not-found"],{9703:function(n,t,e){"use strict";e.r(t);var o=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",[e("h1",[n._v("Страница не найдена")]),e("p",[n._v(" Ой! Нет такой страницы, попробуйте перейти "),e("router-link",{attrs:{to:{name:"Home"}}},[n._v("на главную")])],1)])},r=[],u=e("2877"),l={},a=Object(u["a"])(l,o,r,!1,null,null,null);t["default"]=a.exports}}]);
+//# sourceMappingURL=not-found.2d41179c.js.map
