57f4357822a0a51e456993f11db4e2a44c883f92
Istomina Asya <istomina.asia@yandex.ru>
2021-04-07 14:53:43 +0300

    Updates

--- js/success.0cc2a049.js
+++ js/success.0cc2a049.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["success"],{bb0d:function(e,t,n){"use strict";n.r(t);var u=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("p",[e._v("Запись успешно добавленa")]),n("router-link",{attrs:{to:{name:"Home"}}},[e._v("перейти на главную")])],1)},o=[],s={mounted:function(){var e=this;setTimeout((function(){e.$router.push({name:"Home"})}),2e3)}},r=s,a=n("2877"),c=Object(a["a"])(r,u,o,!1,null,null,null);t["default"]=c.exports}}]);
-//# sourceMappingURL=success.83f45d3e.js.map
+//# sourceMappingURL=success.0cc2a049.js.map
