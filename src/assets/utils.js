6b344dc6ca379f109fb492fe6f8da435af786467
Istomina Asya <istomina.asia@yandex.ru>
2021-04-09 08:24:09 +0300

    11 Fixed date formatting

--- src/assets/utils.js
+++ src/assets/utils.js
@@ -2,18 +2,24 @@ const millisecondsInDay = 60 * 60 * 24 * 1000;
 
 export function formatDate(date) {
   if (!(date instanceof Date)) date = new Date(date);
+
   let today = new Date();
-  let difference = (date - today) / millisecondsInDay;
+  let difference = (today - date) / millisecondsInDay;
   let dayOfDate = date.getDate();
+  let previousDay = new Date(
+    new Date().setDate(new Date().getDate() - 1),
+  ).getDate();
+
   if (difference < 1 && date.getDate() == today.getDate()) return 'сегодня';
-  if (difference < 2 && date.setDate(dayOfDate - 1) == today.getDate())
-    return 'вчера';
+  if (difference < 2 && previousDay == dayOfDate) return 'вчера';
+
   return date.toLocaleString(navigator.language, {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
   });
 }
+
 export function showHeader() {
   document.addEventListener('scroll', () => {
     let header = document.querySelector('.blog__header');
